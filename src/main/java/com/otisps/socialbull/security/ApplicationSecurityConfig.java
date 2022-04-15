1aeeda1f28473abc7d51885c6cece4284dfac638
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-15 20:35:47 +0100

    Version 3.1 Http basic version

--- src/main/java/com/otisps/socialbull/security/ApplicationSecurityConfig.java
+++ src/main/java/com/otisps/socialbull/security/ApplicationSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.otisps.socialbull.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,4 +9,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }
