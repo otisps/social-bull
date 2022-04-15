8cd8d0df26caa78ea7f71a08c89a0f4470e9c289
jokerotis <63875651+jokerotis@users.noreply.github.com>
2022-04-15 14:00:49 +0100

    Version 0 Commit and push...

---
+++ src/main/java/com/otisps/socialbull/SocialBullApplication.java
@@ -0,0 +1,13 @@
+package com.otisps.socialbull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialBullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialBullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/otisps/socialbull/SocialBullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otisps.socialbull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialBullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
